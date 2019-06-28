edcae5f4b401f2c484ddfecf14770a6cf0ee5e53
Mislene <mislenedalila@gmail.com>
2019-06-28 10:11:08 -0300

    conect server 2

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -5,6 +5,7 @@ const structjson = require("./structjson");
 const config = require("../config/keys");
 
 const projectID = config.googleProjectID;
+const sessionID = config.dialogFlowSessionID;
 
 const credentials = {
   client_email: config.googleClientEmail,
@@ -23,6 +24,33 @@ const sessionPath = sessionClient.sessionPath(
 
 module.exports = {
   handleAction: function(responses) {
+    return responses;
+  },
+
+  textQuery: async function(text, userID, parameters = {}) {
+    let self = module.exports;
+    const sessionPath = sessionClient.sessionPath(
+      projectID,
+      sessionID + userID
+    );
+
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: text,
+          languageCode: config.dialogFlowSessionLanguageCode
+        }
+      },
+      queryParams: {
+        payload: {
+          data: parameters
+        }
+      }
+    };
+
+    let responses = await sessionClient.detectIntent(request);
+    responses = await self.handleAction(responses);
     return responses;
   },
   eventQuery: async function(event, parameters = {}) {
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,18 +1,25 @@
+"use strict";
+
 const chatbot = require("../chatbot/chatbot");
 
 module.exports = app => {
-  app.get("/", (req, res) => {
-    res.send({ hello: "Mislene" });
-  });
+  // app.get('/', (req, res) => {
+  //     res.send({'hello': 'Johnny'})
+  // });
 
   app.post("/api/df_text_query", async (req, res) => {
-    let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
+    let responses = await chatbot.textQuery(
+      req.body.text,
+      req.body.userID,
+      req.body.parameters
+    );
     res.send(responses[0].queryResult);
   });
 
   app.post("/api/df_event_query", async (req, res) => {
     let responses = await chatbot.eventQuery(
-      req.body.evet,
+      req.body.event,
+      req.body.userID,
       req.body.parameters
     );
     res.send(responses[0].queryResult);
